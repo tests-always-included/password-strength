1c9cf18865756751697fd4c23053f9ddd8f1b973
Tyler Akins <fidian@rumkin.com>
2021-03-02 16:12:39 -0600

    Add TypeScript, bump dependency versions

---
+++ lib/node.d.ts
@@ -0,0 +1,48 @@
+export interface PassswordStrengthExports {
+    PasswordStrength: PasswordStrength;
+    commonPasswords: string[];
+    trigraphs: PasswordStrengthTrigraphMap;
+}
+
+export class PasswordStrength {
+    charsets: PasswordStrengthGroups;
+    commonPasswords: null | string[];
+    trigraph: null | PasswordStrengthTrigraphMap;
+
+    addCommonPasswords(passwords: string[] | string): this;
+    addTrigraphMap(map: PasswordStrengthTrigraphMap): this;
+    charsetGroups(password: string): { [key: string]: boolean | string };
+    charsetSize(groups: PasswordStrengthGroups): number;
+    check(password: string): PasswordStrengthStatistics;
+    checkCommonPasswords(password: string): boolean;
+    checkTrigraph(password: string, charsetSize: number): number;
+    determineStrength(status: PasswordStrengthStatistics): PasswordStrengthCode;
+    nistScore(password: string): number;
+    otherChars(password: string): string;
+    shannonScore(password: string): number;
+}
+
+export interface PasswordStrengthGroups {
+    [key: string]: string;
+}
+
+export interface PasswordStrengthTrigraphMap {
+    [key: string]: number;
+}
+
+export interface PasswordStrengthStatistics {
+    charsetSize: number;
+    commonPassword: boolean;
+    passwordLength: number;
+    shannonEntropyBits: number;
+    strength: PasswordStrengthCode;
+    trigraphEntropyBits: null | number;
+}
+
+export enum PasswordStrengthCode {
+    VERY_WEAK = "VERY_WEAK",
+    WEAK = "WEAK",
+    REASONABLE = "REASONABLE",
+    STRONG = "STRONG",
+    VERY_STRONG = "VERY_STRONG"
+}
